0ee47dd6f2b026e5fd2986863a19e83fb64069f2
soberdeer <antongraywing@gmail.com>
2023-06-03 21:44:01 +0300

    changed video

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -15,17 +15,17 @@ export function Video({ style, title, src, type, ...others }: VideoProps) {
     <Center
       sx={{ width: '100%' }}
       {...others}
-      dangerouslySetInnerHTML={{
-        __html: `<video controls width="100%" title="${title}">
-        <source src="${src}?origin=https://maitreya-academy.vercel.app/" type="${type}" />
-        Браузер не поддерживает видео
-      </video>`,
-      }}
+      // dangerouslySetInnerHTML={{
+      //   __html: `<video controls width="100%" title="${title}">
+      //   <source src="${src}?origin=https://maitreya-academy.vercel.app/" type="${type}" />
+      //   Браузер не поддерживает видео
+      // </video>`,
+      // }}
     >
-      {/*<video controls width="100%" title={title}>*/}
-      {/*  <source src={`${src}?origin=https://maitreya-academy.vercel.app/`} type={type} />*/}
-      {/*  Браузер не поддерживает видео*/}
-      {/*</video>*/}
+      <video controls width="100%" title={title} playsInline>
+        <source src={`${src}?origin=https://maitreya-academy.vercel.app/`} type="video/mp4" />
+        Браузер не поддерживает видео
+      </video>
     </Center>
   );
 }
